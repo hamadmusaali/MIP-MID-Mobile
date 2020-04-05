9d85e224e2ef0d7f30db7517e34587456bc094f8
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2020-04-05 19:19:34 -0300

    Inserção da tela inicial da pulverização

--- src/pages/Pulverizacao/index.js
+++ src/pages/Pulverizacao/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, TouchableWithoutFeedback, FlatList, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { List } from 'react-native-paper';
+import { FontAwesome } from "@expo/vector-icons";
+import data from '../../../json/dados.json';
 
 export default function Pulverizacao() {
     const navigation = useNavigation();
@@ -13,18 +16,159 @@ export default function Pulverizacao() {
         navigation.navigate('NovaOperacaoPulverizacao');
     };
 
-    return(
-        <View>
-            <View>
-                <TouchableOpacity onPress={() => navigateToOperacao()}>
-                    <Text>Operação de Pulverização</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() => navigateToNovaOperacao()}>
-                    <Text>Nova Operação de Pulverização</Text>
-                </TouchableOpacity>
-            </View>
+    return (
+        <View style={{ flex: 1 }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <ScrollView>
+                    <View>
+                        <List.Section>
+                            <List.Accordion
+                                title="18-10-2016"
+                                style={{ padding: 0 }}
+                                left={props => <List.Icon {...props} icon="table-large" />}
+                                onPress={() => ("")}
+                                theme={{ colors: { primary: 'blue' } }}>
+                                <TouchableWithoutFeedback>
+                                    <View>
+                                        <FlatList
+                                            horizontal
+                                            data={data.mipmid1}
+                                            keyExtractor={item => item.id}
+                                            renderItem={({ item }) => {
+                                                return (
+                                                    <View>
+                                                        <Text style={{ fontWeight: 'bold' }}>Safra</Text>
+                                                        <Text>{item.Safra}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Produtor</Text>
+                                                        <Text>{item.Produtor} </Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Identificação</Text>
+                                                        <Text>{item.Identificação}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Cultivar</Text>
+                                                        <Text>{item.Cultivar}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Responsável Técnico</Text>
+                                                        <Text>{item.ResponsávelTécnico}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Município</Text>
+                                                        <Text>{item.Município}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Ações:</Text>
+                                                    </View>
+                                                );
+                                            }}
+                                        />
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Operações de Pulverização/Aplicação</Text>
+                                            <TouchableOpacity onPress={() => navigateToOperacao()}>
+                                                <FontAwesome name="eye" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Nova Operação de Pulverização</Text>
+                                            <TouchableOpacity onPress={() => navigateToNovaOperacao()}>
+                                                <FontAwesome name="pagelines" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </List.Accordion>
+
+                            <List.Accordion
+                                title="12-10-2018"
+                                style={{ padding: 0 }}
+                                left={props => <List.Icon {...props} icon="table-large" />}
+                                onPress={() => ("")}
+                                theme={{ colors: { primary: 'blue' } }}>
+                                <TouchableWithoutFeedback>
+                                    <View>
+                                        <FlatList
+                                            horizontal
+                                            data={data.mipmid2}
+                                            keyExtractor={item => item.id}
+                                            renderItem={({ item }) => {
+                                                return (
+                                                    <View>
+                                                        <Text style={{ fontWeight: 'bold' }}>Safra</Text>
+                                                        <Text>{item.Safra}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Produtor</Text>
+                                                        <Text>{item.Produtor} </Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Identificação</Text>
+                                                        <Text>{item.Identificação}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Cultivar</Text>
+                                                        <Text>{item.Cultivar}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Responsável Técnico</Text>
+                                                        <Text>{item.ResponsávelTécnico}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Município</Text>
+                                                        <Text>{item.Município}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Ações:</Text>
+                                                    </View>
+                                                );
+                                            }}
+                                        />
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Operações de Pulverização/Aplicação</Text>
+                                            <TouchableOpacity onPress={() => navigateToOperacao()}>
+                                                <FontAwesome name="eye" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Nova Operação de Pulverização</Text>
+                                            <TouchableOpacity onPress={() => navigateToNovaOperacao()}>
+                                                <FontAwesome name="pagelines" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </List.Accordion>
+
+                            <List.Accordion
+                                title="16-10-2019"
+                                style={{ padding: 0 }}
+                                left={props => <List.Icon {...props} icon="table-large" />}
+                                onPress={() => ("")}
+                                theme={{ colors: { primary: 'blue' } }}>
+                                <TouchableWithoutFeedback>
+                                    <View>
+                                        <FlatList
+                                            horizontal
+                                            data={data.mipmid3}
+                                            keyExtractor={item => item.id}
+                                            renderItem={({ item }) => {
+                                                return (
+                                                    <View>
+                                                        <Text style={{ fontWeight: 'bold' }}>Safra</Text>
+                                                        <Text>{item.Safra}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Produtor</Text>
+                                                        <Text>{item.Produtor} </Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Identificação</Text>
+                                                        <Text>{item.Identificação}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Cultivar</Text>
+                                                        <Text>{item.Cultivar}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Responsável Técnico</Text>
+                                                        <Text>{item.ResponsávelTécnico}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Município</Text>
+                                                        <Text>{item.Município}</Text>
+                                                        <Text style={{ fontWeight: 'bold' }}>Ações:</Text>
+                                                    </View>
+                                                );
+                                            }}
+                                        />
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Operações de Pulverização/Aplicação</Text>
+                                            <TouchableOpacity onPress={() => navigateToOperacao()}>
+                                                <FontAwesome name="eye" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                        <View>
+                                            <Text style={{ fontWeight: 'bold' }}>Nova Operação de Pulverização</Text>
+                                            <TouchableOpacity onPress={() => navigateToNovaOperacao()}>
+                                                <FontAwesome name="pagelines" size={20} />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </List.Accordion>
+                        </List.Section>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         </View>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
+import { Button, Text, View, Platform, SafeAreaView, StatusBar} from 'react-native';
 import { NavigationContainer, DrawerActions } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerItem, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
 
 import Home from './pages/Home';
 import GerenciamentoURs from './pages/GerenciamentoURs';
@@ -19,17 +20,69 @@ import NovaOperacaoPulverizacao from './pages/Pulverizacao/NovaOperacaoPulveriza
 const Drawer = createDrawerNavigator();
 const AppStack = createStackNavigator();
 
+function CustomDrawerContent(props) {
+  return (  
+    <DrawerContentScrollView {...props}>
+      <SafeAreaView style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0}}>
+      <View style={{ height: 50, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{fontWeight: 'bold', fontSize: 18 }}>MIP/MID</Text>
+      </View>
+      <DrawerItemList {...props} 
+      activeTintColor='white'
+      activeBackgroundColor='navy'
+      inactiveTintColor='navy'
+      />
+      <DrawerItem
+        label="Teste"
+        onPress={() => {}}
+      />
+      </SafeAreaView>
+    </DrawerContentScrollView>
+    
+  );
+}
+
 /*Menu Lateral*/
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={StackHome} />
-        <Drawer.Screen name="Gerenciamento URs" component={StackGerenciamentoURs} />
-        <Drawer.Screen name="Gerenciamento Pesquisa" component={StackGerenciamentoPesquisa} />
-        <Drawer.Screen name="Anotações De Campo" component={StackAnotacoesDeCampo} />
-        <Drawer.Screen name="Monitoramento Ferrugem" component={StackMonitoramentoFerrugem} />
-        <Drawer.Screen name="Pulverização" component={StackPulverizacao} />
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerContent={props => <CustomDrawerContent {...props} />}
+        drawerStyle={{
+          backgroundColor: 'white',
+          width: 240,
+        }}>
+        <Drawer.Screen name="Home" component={StackHome}
+          options={{
+            drawerIcon: (({focused, color, size }) => <MaterialIcons name="home" size={20} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
+        <Drawer.Screen name="Gerenciamento URs" component={StackGerenciamentoURs}
+          options={{
+            drawerIcon: (({focused, color, size }) => <MaterialIcons name="people" size={21} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
+        <Drawer.Screen name="Gerenciamento Pesquisa" component={StackGerenciamentoPesquisa}
+          options={{
+            drawerIcon: (({focused, color, size }) => <MaterialIcons name="book" size={21} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
+        <Drawer.Screen name="Anotações De Campo" component={StackAnotacoesDeCampo}
+          options={{
+            drawerIcon: (({focused, color, size }) => <MaterialIcons name="bug-report" size={21} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
+        <Drawer.Screen name="Monitoramento Ferrugem" component={StackMonitoramentoFerrugem}
+          options={{
+            drawerIcon: (({focused, color, size }) => <FontAwesome name="leaf" size={20} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
+        <Drawer.Screen name="Pulverização" component={StackPulverizacao}
+          options={{
+            drawerIcon: (({focused, color, size }) => <MaterialIcons name="filter-vintage" size={21} color={focused ? 'white' : 'navy'} />)
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
